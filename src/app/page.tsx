a0c7d852c7d8042908a01cd988b9ec35c08c3f15
Zulqs <zulqs@apps.ipb.ac.id>
2024-03-31 06:32:30 +0700

    Update globals.css and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Nav from "../components/nav";
+import Nav from "@/components/nav";
+import Hero from "@/components/hero";
+import Content from "@/components/content";
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <Hero />
+      <Content />
     </>
   );
 }
